8843c7700a720234d430341a5d2f1f03aaac9d69
Théo <t-henon@hotmail.fr>
2024-05-12 22:13:22 +0200

    chore: install 'boostrap' package

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 document.querySelector("#app").innerHTML = `
-    <p>Hello World!</p>
-`
+    <h1>Hello World!</h1>
+    <p>You are a beautiful person!</p>
+`;
