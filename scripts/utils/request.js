c12f073535c38ad211cac5396dd65e01ac56bbc5
Théo <t-henon@hotmail.fr>
2024-05-18 14:42:06 +0200

    fix(Request): surround 'post' method code with a try/catch when 'fetch' fail

--- scripts/utils/request.js
+++ scripts/utils/request.js
@@ -2,12 +2,17 @@ export class Request {
     static API_URL = "http://localhost:5000";
 
     static async post(route, body) {
-        return await fetch(`${Request.API_URL}/${route}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        });
+        try {
+            return await fetch(`${Request.API_URL}/${route}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            });
+        } catch (e) {
+            alert("Cannot connect to the server. Make sure that the server is running.");
+            return null;
+        }
     }
 }
