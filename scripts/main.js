d64725df2d892e050cdd1ee9007a4b25dd2e3dc8
Théo <t-henon@hotmail.fr>
2024-05-21 18:21:46 +0200

    feat: enable Bootstrap tooltips

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import bootstrap from 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import {App} from "./app.js";
 
 
 const app = new App();
+
+
+// Enable tooltips
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
