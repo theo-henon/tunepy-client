6390ee394377a6911691c37c630f8ccedccb6a81
LeLapinouAlpha <t-henon@hotmail.fr>
2024-05-24 12:39:37 +0200

    feat(ServerSelectionPage): add actions button in list group items

--- src/index.js
+++ src/index.js
@@ -19,3 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
 // Enable popovers
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+// Enable collapse
+const collapseElementList = document.querySelectorAll('.collapse')
+const collapseList = [...collapseElementList].map(collapseEl => new bootstrap.Collapse(collapseEl))
--- src/js/pages/serverSelectionPage.js
+++ src/js/pages/serverSelectionPage.js
@@ -1,31 +1,37 @@
 export class ServerSelectionPage extends HTMLDivElement {
     constructor() {
         super();
+        this.classList.add("d-flex", "flex-column", "align-items-center", "gap-3");
 
         this.innerHTML = `
             <h1>Server selection</h1>
             <button class="btn btn-primary"><i class="bi bi-plus-circle"></i> Add a server</button>
             <div class="list-group w-50">
-                <a href="#" class="list-group-item list-group-item-action">
+                <a class="list-group-item list-group-item-action" data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">
                     <div class="container">
                         <div class="row">
                             <div class="col d-flex">
                                 <p>
-                                    <strong style="font-size: 1.5em">Main server</strong><br>
+                                    <strong style="font-size: 1.5em">Main server</strong>
+                                    <i style="font-size: 1.75em;" class="bi bi-check text-success" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-title="Ping: 1ms"></i><br>
                                     localhost:8080
                                 </p>                              
                             </div>
-                             <div class="col d-flex align-items-center">
-                                <div class="ms-auto">                                
-                                    <i class="bi bi-check text-success" data-bs-toggle="popover" data-bs-placement="top" data-bs-title="Server status" data-bs-content="Ping: 1ms"></i>
-                                    <i class="bi bi-arrow-right"></i>
-                                </div>
-                             </div>
+                        </div>
+                    </div>
+                    
+                    <div class="collapse" id="collapse1">
+                        <div class="card card-body gap-1">
+                            <button class="btn btn-success"><i class="bi bi-arrow-right"></i> Select</button>
+                            <button class="btn btn-primary"><i class="bi bi-pen"></i> Edit</button>
+                            <button class="btn btn-danger"><i class="bi bi-trash"></i> Remove</button>
                         </div>
                     </div>
                 </a>
             </div>
         `;
+
+        this.querySelectorAll(".collapse button").forEach(btn => btn.addEventListener("click", () => alert("toto")));
     }
 }
 
